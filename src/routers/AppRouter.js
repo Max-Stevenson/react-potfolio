788c1297d31ee3b40b9fd25cd2ed99f15c0aa60e
Max Stevenson <maxstevenson@msn.com>
2020-01-19 21:54:17 +0000

    Portfolio mockup links implemented.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import { createBrowserHistory } from 'history';
 import HomePage from '../components/HomePage';
 import ContactPage from '../components/ContactPage';
 import PortfolioItemPage from '../components/PortfolioItemPage';
+import PorfolioPage from '../components/PortfolioPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
@@ -15,6 +16,7 @@ const AppRouter = () => (
 		<div>
 			<Switch>
 				<Route path="/" component={HomePage} exact={true}/>
+				<Route path="/portfolio" component={PorfolioPage} exact={true}/>
 				<Route path="/portfolio/:id" component={PortfolioItemPage}/>
 				<Route path="/contact" component={ContactPage}/>
 				<Route component={NotFoundPage} />
